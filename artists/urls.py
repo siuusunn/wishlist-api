841af6943c6b672a79276b07249776a2b81b8a9f
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-06-02 19:36:14 +0200

    Fixed url. Single user view now available

--- artists/urls.py
+++ artists/urls.py
@@ -3,5 +3,5 @@ from .views import ArtistListView, ArtistDetailView
 
 urlpatterns = [
   path('', ArtistListView.as_view()),
-  path('<int:pk>', ArtistDetailView.as_view())
+  path('<int:pk>/', ArtistDetailView.as_view())
 ]
