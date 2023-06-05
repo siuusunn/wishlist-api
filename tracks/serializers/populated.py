0f5b27d8db7ae568966bb9774c1361f5da4e4d5f
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-06-05 15:15:59 +0200

    Migrations fix

--- tracks/serializers/populated.py
+++ tracks/serializers/populated.py
@@ -1,6 +1,8 @@
 # from .common import TrackSerializer
 from .common import TrackSerializer
 from jwt_auth.serializers.common import UserSerializer # this uses absolute path. Absolute paths in django have the manage.py as the root
+from artists.serializers.common import ArtistSerializer
 
 class PopulatedTrackSerializer(TrackSerializer):
-  users = UserSerializer(many=True)
+  # users = UserSerializer(many=True)
+  artist = ArtistSerializer(many=True)
