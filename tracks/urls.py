b27e0970c21e9f8f3d45f1366c5b40053ebba98f
Alice Lo <115280336+siuusunn@users.noreply.github.com>
2023-06-05 16:43:04 +0200

    Added Search function

--- tracks/urls.py
+++ tracks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TrackListView, TrackDetailView
+from .views import TrackListView, TrackDetailView, TrackSearchView
 
 urlpatterns = [
     path('', TrackListView.as_view()),
-    path('<int:pk>/', TrackDetailView.as_view())
+    path('<int:pk>/', TrackDetailView.as_view()),
+    path('search/', TrackSearchView.as_view())
 ]
